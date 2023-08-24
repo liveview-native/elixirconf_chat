c35adc8ee1fa81e3f5388afaaf0eb7522a45aaba
Diana Le <diana.le@dockyard.com>
2023-08-24 17:28:39 -0500

    Initial styling for schedule sidebar

--- lib/elixirconf_chat_web/live/chat_live.ex
+++ lib/elixirconf_chat_web/live/chat_live.ex
@@ -75,13 +75,20 @@ defmodule ElixirconfChatWeb.ChatLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="w-full">
-      <%= if @room_page do %>
-        <.room_page {assigns} />
-      <% end %>
-      <.logo height={48} width={48} />
-      <.hallway {assigns} />
-      <.rooms_list {assigns} />
+    <div class="px-4">
+      <.logo />
+      <div class="mx-auto w-full max-w-[1200px] md:grid md:grid-cols-6 border border-brand-gray-200 rounded-t-[32px]">
+        <div class="md:col-span-3 md:border-r md:border-brand-gray-200 lg:col-span-2 p-4 min-[448px]:p-6">
+          <h1 class="font-medium text-2xl min-[448px]:text-3.5xl text-brand-gray-700">Schedule</h1>
+          <.hallway {assigns} />
+          <.rooms_list {assigns} />
+        </div>
+        <div class="md:col-span-3 lg:col-span-4">
+          <%= if @room_page do %>
+            <.room_page {assigns} />
+          <% end %>
+        </div>
+      </div>
     </div>
     """
   end
@@ -164,7 +171,7 @@ defmodule ElixirconfChatWeb.ChatLive do
 
   def logo(assigns) do
     ~H"""
-    LOGO TODO
+    <img class="my-5 min-[448px]:mt-8 min-[448px]:mb-7 mx-auto h-10 w-auto" src="/images/elixir-logo.png" width="75" height="60" alt="" />
     """
   end
 
@@ -433,17 +440,13 @@ defmodule ElixirconfChatWeb.ChatLive do
 
   def hallway_item(assigns) do
     ~H"""
-    <div class="background:rect ph-24">
-      <div class="fg-color:lightchrome opacity-0.25" template={:rect} corner-radius="16" />
+    <article class="mt-5 p-3 bg-brand-gray-50 rounded-2xl">
       <div spacing={0} phx-click="join_room" phx-value-room-id={"#{@room.id}"}>
-        <br />
-        <div class="h-10 w-10 p-10 fg-color:forestgreen" />
-        <p class="font-subheadline font-weight-semibold h-48 capitalize kerning-3">
+        <h2 class="uppercase font-semibold text-sm text-brand-gray-700 tracking-[3px] text-center">
           <%= @room.title %>
-        </p>
-        <br />
+        </h2>
       </div>
-    </div>
+    </article>
     """
   end
 
@@ -477,17 +480,13 @@ defmodule ElixirconfChatWeb.ChatLive do
   def rooms_list(assigns) do
     ~H"""
     <%= for {day, timeslots} <- @sorted_days do %>
-      <div pinned-views="section-headers">
-        <section>
-          <div template={:content}>
+      <div>
+        <section class="mt-6" aria-labelledby="schedule-day">
+          <h2 class="text-xl min-[448px]:text-2xl text-brand-gray-800" id="schedule-day"><%= day %></h2>
+          <div class="mt-3 space-y-3">
             <%= for timeslot <- timeslots do %>
               <.timeslot_item timeslot={timeslot} track_labels={@track_labels} />
             <% end %>
-          </div>
-          <div class="background:rect" template={:header}>
-            <div class="fg-color:bgcolor" template={:rect} />
-            <p class="font-title h-48 ph-24"><%= day %></p>
-            <br />
           </div>
         </section>
       </div>
@@ -578,52 +577,45 @@ defmodule ElixirconfChatWeb.ChatLive do
 
   def timeslot_item(assigns) do
     ~H"""
-    <div class="background:rect ph-24 align-leading image-scale-small">
-      <div class="fg-color:lightchrome opacity-0.25" template={:rect} corner-radius="16" />
-      <div spacing={16} class="p-16">
-        <div>
-          <p class="font-subheadline type-size-x-small font-weight-semibold h-12 capitalize kerning-2 opacity-0.825">
-            <%= @timeslot.formatted_string %>
-          </p>
-          <br />
-        </div>
+    <article class="p-3 bg-brand-gray-50 rounded-2xl">
+      <div>
+        <h3 class="mb-3 uppercase font-semibold text-sm text-brand-gray-700 tracking-[3px]">
+          <%= @timeslot.formatted_string %>
+        </h3>
         <%= for room <- @timeslot.rooms do %>
           <div>
-            <div>
-              <%= if room.track > 0 do %>
-                <div class="fg-color-secondary h-24 opacity-0.75 overlay:rect">
-                  <div class="stroke-secondary fg-color-clear" template={:rect} corner-radius="8" />
-                  <p class="capitalize p-8 kerning-4 font-subheadline type-size-x-small font-weight-semibold offset-x-2">
-                    Track <%= Map.get(@track_labels, room.track, "?") %>
-                  </p>
-                </div>
-                <br />
-              <% end %>
-            </div>
-            <div phx-click="join_room" phx-value-room-id={"#{room.id}"} spacing={8}>
-              <div spacing={8}>
-                <div>
-                  <p class="font-headline font-weight-semibold"><%= room.title %></p>
-                  <br />
-                </div>
-                <%= if room.presenters != [] do %>
-                  <div>
-                    <p class="font-subheadline opacity-0.825">
-                      <%= Enum.join(room.presenters, ", ") %>
-                    </p>
-                    <br />
+            <%= if room.track > 0 do %>
+              <span class="inline-block mb-2 px-3 py-1.5 rounded-lg border border-brand-gray-300 font-semibold text-brand-gray-500 text-xs uppercase tracking-[3px]">
+                Track <%= Map.get(@track_labels, room.track, "?") %>
+              </span>
+            <% end %>
+            <div class="cursor-pointer" phx-click="join_room" phx-value-room-id={"#{room.id}"}>
+              <%= if room.presenters != [] do %>
+                <div class="flex gap-x-2 items-start">
+                  <div class="shrink-0 h-10 w-10 flex items-center justify-center rounded-full bg-white">
+                    <svg class="fill-brand-purple" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path d="M4 22C4 17.5817 7.58172 14 12 14C16.4183 14 20 17.5817 20 22H18C18 18.6863 15.3137 16 12 16C8.68629 16 6 18.6863 6 22H4ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11Z"></path></svg>
                   </div>
-                <% end %>
-              </div>
-              <div class="opacity-0.75 type-size-x-small">
-                <img system-name="person.2" />
-                <p>0</p>
-              </div>
+                  <div class="grow">
+                    <p class="font-medium text-xl/6 text-brand-gray-800"><%= room.title %></p>
+                    <div class="mt-1 mb-4 flex items-center justify-between">
+                      <p class="leading-5 text-brand-gray-600">
+                        <%= Enum.join(room.presenters, ", ") %>
+                      </p>
+                      <div class="flex items-center gap-x-2">
+                        <svg class="w-4 h-4 fill-brand-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path d="M2 22C2 17.5817 5.58172 14 10 14C14.4183 14 18 17.5817 18 22H16C16 18.6863 13.3137 16 10 16C6.68629 16 4 18.6863 4 22H2ZM10 13C6.685 13 4 10.315 4 7C4 3.685 6.685 1 10 1C13.315 1 16 3.685 16 7C16 10.315 13.315 13 10 13ZM10 11C12.21 11 14 9.21 14 7C14 4.79 12.21 3 10 3C7.79 3 6 4.79 6 7C6 9.21 7.79 11 10 11ZM18.2837 14.7028C21.0644 15.9561 23 18.752 23 22H21C21 19.564 19.5483 17.4671 17.4628 16.5271L18.2837 14.7028ZM17.5962 3.41321C19.5944 4.23703 21 6.20361 21 8.5C21 11.3702 18.8042 13.7252 16 13.9776V11.9646C17.6967 11.7222 19 10.264 19 8.5C19 7.11935 18.2016 5.92603 17.041 5.35635L17.5962 3.41321Z"></path></svg>
+                        <p class="leading-5 text-brand-gray-600">0</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              <% else %>
+                <p class="font-medium text-xl/6 text-brand-gray-800"><%= room.title %></p>
+              <% end %>
             </div>
           </div>
         <% end %>
       </div>
-    </div>
+    </article>
     """
   end
 
