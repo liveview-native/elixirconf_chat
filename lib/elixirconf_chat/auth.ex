15a9faf4b789c05f1eaead4db1e628db4ba731bb
May Matyi <may@matyi.net>
2023-08-25 12:03:57 -0700

    Allow disable code randomization on login for test users

--- lib/elixirconf_chat/auth.ex
+++ lib/elixirconf_chat/auth.ex
@@ -7,13 +7,17 @@ defmodule ElixirconfChat.Auth do
   @doc """
   Randomizes the login code for a User and updates its
   `login_code_expires_at` field to be 1 hour from now.
+  This function is only called when the User has the
+  `randomize_code_on_login` field set to `true`.
   """
-  def randomize_user_login_code(%User{} = user) do
+  def randomize_user_login_code(%User{randomize_code_on_login: true} = user) do
     Users.update_user(user, %{
       login_code: LoginCodes.random_login_code(),
       login_code_expires_at: LoginCodes.expires_at()
     })
   end
+
+  def randomize_user_login_code(user), do: {:ok, user}
 
   @doc """
   Generates a session token for a User given its ID.
